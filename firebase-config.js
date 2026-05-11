b9fbc23719cb6348a9ad86e053f09c89efc265d6
kimsunmin061138-beep <kimsunmin061138@gmail.com>
2026-05-11 12:55:08 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDw9lNbhNPt4ja_jWF6WL6TWzxSrOzWXkg",
+  authDomain: "project-2453934869940028901.firebaseapp.com",
+  projectId: "project-2453934869940028901",
+  storageBucket: "project-2453934869940028901.firebasestorage.app",
+  messagingSenderId: "533917282731",
+  appId: "1:533917282731:web:a74f8640a78ef39aa422ee",
+  measurementId: "G-5WVL5QTK3H"
+};
+
+export default firebaseConfig;
